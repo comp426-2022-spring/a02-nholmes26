0e8bb93be2402ff57af81a0d94bf301aa4f1dbaf
Nathan Holmes <nholmes@email.unc.edu>
2022-02-11 00:43:47 -0500

    coinFlip case sensitive

--- modules/coin.mjs
+++ modules/coin.mjs
@@ -18,9 +18,9 @@ export function coinFlip() {
   var result;
   var x = Math.floor(Math.random() * 2); //generate a 1 or a 0 to represent Heads or Tails
   if (x == 1) {
-    result = "Heads";
+    result = "heads";
   } else {
-    result = "Tails";
+    result = "tails";
   }
   return result;
 }
